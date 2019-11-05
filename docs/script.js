c54da993655476ec88ff25d10f619a42e0ae30f9
Remlow <52727449+Remlow@users.noreply.github.com>
2019-11-05 23:12:36 +1300

    245

--- docs/script.js
+++ docs/script.js
@@ -35,12 +35,3 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 }
 
-var navTrigger = document.getElementsByClassName('nav-trigger')[0],
-    body = document.getElementsByTagName('body')[0];
-
-navTrigger.addEventListener('click', toggleNavigation);
-
-function toggleNavigation(event) {
-  event.preventDefault();
-  body.classList.toggle('nav-open');
-}
