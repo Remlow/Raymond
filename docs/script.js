9736feadb2d8acc6a69152a5974537a28624d67e
Remlow <52727449+Remlow@users.noreply.github.com>
2019-11-05 21:05:16 +1300

    228

--- docs/script.js
+++ docs/script.js
@@ -30,7 +30,7 @@ function openNav() {
   }
   
 function closeNav() {
-document.getElementById("mySidenav").style.width = "0";
-document.getElementById("main").style.marginLeft= "0";
-document.body.style.backgroundColor = "white";
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
 }
