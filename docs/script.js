be19ad61e6870cac616ac8faf73271d5e51ff2ae
Remlow <52727449+Remlow@users.noreply.github.com>
2019-11-19 17:33:20 +1300

    19/11/19

--- docs/script.js
+++ docs/script.js
@@ -1,18 +1,3 @@
-if (screen.width >= 1024) {
-    window.onscroll = function() {stickynav()};
-    
-    var header = document.getElementById("myTopnav");
-    var sticky = header.offsetTop;
-    
-    function stickynav() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-    }
-}
-
 
 function myFunction() {
   var x = document.getElementById("myLinks");
@@ -35,4 +20,16 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 }
 
+if(screen.width > 1024)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
 
