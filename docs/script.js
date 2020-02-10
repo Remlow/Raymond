1469e916b03b4c167ebbb96ff3be36842b34618d
Remlow <52727449+Remlow@users.noreply.github.com>
2020-02-10 16:30:17 +1300

    10/02/20

--- docs/script.js
+++ docs/script.js
@@ -1,13 +1,4 @@
 
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -20,16 +11,4 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 }
 
-if(screen.width > 1024)
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
-  } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
-  }
-}
 
